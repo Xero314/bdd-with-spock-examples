610b5621be2aa8fb0e4d612ddeef1334b6823bf6
Jamie Smyth <jamie.smyth@bodybuilding.com>
2016-01-17 23:46:33 -0800

    Define Feature Criteria

--- src/test/groovy/com/bodybuilding/bdd/DieSpec.groovy
+++ src/test/groovy/com/bodybuilding/bdd/DieSpec.groovy
@@ -5,5 +5,9 @@ import spock.lang.Specification
 class DieSpec extends Specification {
     def "Rolling a Die should produce a random number no less than 1 and no greater than the number of facets of the die" ()
     {
+        given: "a die"
+        when: "die is rolled"
+        then: "result is greater than or equal to 1"
+        and: "result is less than or equal to number of facets on the die"
     }
 }
