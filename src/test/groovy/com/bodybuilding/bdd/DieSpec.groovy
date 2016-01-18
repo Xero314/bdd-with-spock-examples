727ebdf6fa93d099a288e590e9b99367efb94d46
Jamie Smyth <jamie.smyth@bodybuilding.com>
2016-01-17 23:45:02 -0800

    Define the Feature Method

--- src/test/groovy/com/bodybuilding/bdd/DieSpec.groovy
+++ src/test/groovy/com/bodybuilding/bdd/DieSpec.groovy
@@ -3,5 +3,7 @@ package com.bodybuilding.bdd
 import spock.lang.Specification
 
 class DieSpec extends Specification {
-
+    def "Rolling a Die should produce a random number no less than 1 and no greater than the number of facets of the die" ()
+    {
+    }
 }
